4e25779a06fc1bb087b7e7075735672c4c1c897d
anhpt-0250 <pham.tuan.anh.c@sun-asterisk.com>
2023-09-05 16:12:25 +0700

    Init Layout

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,7 @@
+class Admin::BaseController < ApplicationController
+  # tao controller admin base
+  # de tat ca cac controller cua admin se ke thua tu controller nay
+  # controller nay de viet cac function dung chung cho cac controller admin
+  # VD: check  la admin ms duoc vao site, ....
+  layout "layouts/application_admin"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    root "pages#home"
+    get "/books", to: "pages#books"
+
+    namespace :admin do
+      root to: "static_pages#index"
+      resources :static_pages
+    end
   end
 end
