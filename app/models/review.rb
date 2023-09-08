8d95173ea862698c5bbeee02d2f6bd3f45e665ff
ngoctt-0375 <tran.tien.ngoc@sun-asterisk.com>
2023-09-08 15:21:30 +0700

    Review book

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
+  validates :rating, presence: true
+  validates :comment, presence: true
   delegate :name, to: :user, prefix: true, allow_nil: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#index"
+    get "/static_pages/index"
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
     resources :users
@@ -7,6 +9,10 @@ Rails.application.routes.draw do
     post "/login", to: "sessions#create"
     get "/logout", to: "sessions#destroy"
     delete "/logout", to: "sessions#destroy"
+
+    resources :books, only: %i(show new create) do
+      resources :reviews
+    end
 
     namespace :admin do
       root to: "static_pages#index"
@@ -16,8 +22,6 @@ Rails.application.routes.draw do
       resources :publishers, only: :index
       resources :categories, only: [:index, :new, :create]
     end
-    root "static_pages#index"
-    get "static_pages/index"
     resources :books, only: :show
 
     resources :authors
