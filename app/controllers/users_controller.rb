b97d90d0682cd1d9f3376f637486036c0bef0c7b
anhpt-0250 <pham.tuan.anh.c@sun-asterisk.com>
2023-09-08 16:33:23 +0700

    Fix conflict + Edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: %i(new create)
   before_action :find_user, except: %i(new create)
+  before_action :correct_user, only: %i(edit update)
 
   def show; end
 
@@ -18,6 +20,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t("profile_updated")
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
@@ -31,5 +44,19 @@ class UsersController < ApplicationController
 
     flash[:danger] = t "not_found"
     redirect_to root_path
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    flash[:danger] = t "please_login"
+    redirect_to login_url
+  end
+
+  def correct_user
+    return if current_user?(@user)
+
+    flash[:error] = t "you_cannot"
+    redirect_to root_url
   end
 end
