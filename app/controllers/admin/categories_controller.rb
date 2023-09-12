bb4a966fad6fb5a6f62eed5f5aab7872e89f603c
hotuanhung <hungchv16@gmail.com>
2023-09-12 10:21:47 +0700

    done1

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,9 +6,28 @@ class Admin::CategoriesController < Admin::BaseController
     )
   end
 
-  def new; end
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = t("admin.category.create_success")
+      redirect_to admin_categories_path
+    else
+      flash[:warning] = t("admin.category.create_fail")
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @category = Category.new
+  end
 
   def edit; end
 
   def destroy; end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:category_name)
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 class Category < ApplicationRecord
   has_many :books_categories, dependent: :destroy
   has_many :books, through: :books_categories
+  validates :category_name, presence: true, uniqueness: true,
+                            length: {maximum: Settings.category.max}
+
   scope :filtered_by_name, lambda {|name|
     where("category_name LIKE ?", "%#{name}%") if name.present?
   }
