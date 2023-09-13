c25e548eb4209fdd26e76bfe915a6941440ac949
hotuanhung <hungchv16@gmail.com>
2023-09-13 13:54:29 +0700

    1/2/3/4

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < Admin::BaseController
+  before_action :find_category, except: %i(index new create)
   def index
     @categories = Category.filtered_by_name(params[:category_search]).paginate(
       page: params[:page],
@@ -21,13 +22,40 @@ class Admin::CategoriesController < Admin::BaseController
     @category = Category.new
   end
 
+  def show; end
+
   def edit; end
 
-  def destroy; end
+  def update
+    if @category.update(category_params)
+      flash[:success] = t("admin.category.update_success")
+      redirect_to admin_categories_path
+    else
+      render :edit, status: :unprocessable_entity
+      flash[:warning] = t("admin.category.update_fail")
+    end
+  end
+
+  def destroy
+    if @category.destroy
+      flash[:success] = t("admin.category.destroy_success")
+    else
+      flash[:warning] = t("admin.category.destroy_fail")
+    end
+    redirect_to admin_categories_path
+  end
 
   private
 
   def category_params
     params.require(:category).permit(:category_name)
+  end
+
+  def find_category
+    @category = Category.find_by id: params[:id]
+    return if @category
+
+    flash[:danger] = t("admin.category.not_found")
+    redirect_to admin_categories_path
   end
 end
