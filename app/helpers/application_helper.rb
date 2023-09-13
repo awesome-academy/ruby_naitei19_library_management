b040e614c08047b292716cd81161b0b4e8ad4146
ngoctt-0375 <tran.tien.ngoc@sun-asterisk.com>
2023-09-13 17:25:04 +0700

    Setup new template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     base_title = I18n.t "title"
     page_title.blank? ? base_title : "#{page_title} | #{base_title}"
   end
+
+  def formatted_datetime datetime
+    datetime.strftime("%H:%M %d/%m/%Y")
+  end
 end
