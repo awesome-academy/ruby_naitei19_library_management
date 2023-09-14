40d1cf7bce1065696660ab71f12d3b30f777decc
hotuanhung <hungchv16@gmail.com>
2023-09-14 09:38:39 +0700

    12

--- app/controllers/admin/publishers_controller.rb
+++ app/controllers/admin/publishers_controller.rb
@@ -1,4 +1,5 @@
 class Admin::PublishersController < Admin::BaseController
+  before_action :find_publisher, except: %i(index new create)
   def index
     name = params[:publisher_search] || ""
     @publishers = Publisher.filtered_by_name(name).paginate(page: params[:page],
@@ -23,11 +24,38 @@ class Admin::PublishersController < Admin::BaseController
 
   def edit; end
 
-  def destroy; end
+  def show; end
+
+  def update
+    if @publisher.update(publisher_params)
+      flash[:success] = t("admin.publisher.update_success")
+      redirect_to admin_publisher_path(@publisher)
+    else
+      render :edit, status: :unprocessable_entity
+      flash[:warning] = t("admin.publisher.update_fail")
+    end
+  end
+
+  def destroy
+    if @publisher.destroy
+      flash[:success] = t("admin.publisher.destroy_success")
+    else
+      flash[:warning] = t("admin.publisher.destroy_fail")
+    end
+    redirect_to admin_publishers_path
+  end
 
   private
 
   def publisher_params
     params.require(:publisher).permit(:name, :email, :address)
+  end
+
+  def find_publisher
+    @publisher = Publisher.find_by id: params[:id]
+    return if @publisher
+
+    flash[:danger] = t("admin.publisher.not_found")
+    redirect_to admin_publishers_path
   end
 end
