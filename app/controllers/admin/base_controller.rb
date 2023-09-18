65d0fd364819a0d80cd1d01c222bb09ec5d7a07b
hotuanhung <hungchv16@gmail.com>
2023-09-18 10:55:08 +0700

    1/2/3/411

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,13 @@
 class Admin::BaseController < ApplicationController
-  # tao controller admin base
-  # de tat ca cac controller cua admin se ke thua tu controller nay
-  # controller nay de viet cac function dung chung cho cac controller admin
-  # VD: check  la admin ms duoc vao site, ....
   layout "layouts/application_admin"
+  before_action :check_admin_role
+
+  private
+
+  def check_admin_role
+    return if current_user&.admin?
+
+    flash[:alert] = t("not_admin")
+    redirect_to root_path
+  end
 end
