1cd720930433a1d9191e9e6533d567d7e4c817b4
ngoctt-0375 <tran.tien.ngoc@sun-asterisk.com>
2023-09-19 21:08:23 +0700

    Fix Header user and display following book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,11 @@
 class BooksController < ApplicationController
   before_action :find_book, only: %i(show)
+  def index
+    @books = Book.search_all(params[:search] || "")
+                 .distinct.paginate(page: params[:page],
+                                    per_page: Settings.page.limit_items)
+  end
+
   def show; end
 
   private
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,30 @@
 class StaticPagesController < ApplicationController
   def index
-    @books = Book.search_all(params[:search] || "")
-                 .distinct.paginate(page: params[:page],
-                                    per_page: Settings.page.limit_items)
+    if logged_in?
+      @followed_books = get_followed_books
+      @books = search_followed_books(@followed_books)
+    else
+      @books = Book.search_all(params[:search] || "")
+                   .distinct.paginate(page: params[:page],
+                                      per_page: Settings.page.limit_items)
+    end
+  end
+
+  private
+
+  def get_followed_books
+    @followed_publishers = current_user.followed_publishers
+    @followed_authors = current_user.followed_authors
+
+    followed_publishers_books = @followed_publishers.map(&:books).flatten
+    followed_authors_books = @followed_authors.map(&:books).flatten
+    followed_publishers_books + followed_authors_books
+  end
+
+  def search_followed_books followed_books
+    Book.search_all(params[:search] || "")
+        .where(id: followed_books.pluck(:id))
+        .distinct.paginate(page: params[:page],
+                           per_page: Settings.page.limit_items)
   end
 end
